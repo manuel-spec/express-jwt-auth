cc496aac85feeee09d094c799017d19e53aa059b
manuel-spec <amaniase084@gmail.com>
2023-12-30 20:48:51 +0300

    .env.example

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require("mongoose")
+var cookieParser = require("cookie-parser")
+
 require('dotenv').config()
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser())
 
 const uri = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@cluster0.dtbumxa.mongodb.net/" + process.env.DB_NAME
 mongoose.connect(uri).then((result) => console.log("DATABASE CONNECTION ESTABLISHED !"))
@@ -43,5 +46,7 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,13 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
 const { validationResult } = require('express-validator')
+const jwt = require('jsonwebtoken')
+const { token } = require('morgan')
+
+const expiresAt = 3 * 60 * 60 * 24
+const createToken = (id) => {
+    return jwt.sign(id, "secret", { expiresIn: expiresAt })
+}
 
 const login = (request, response) => {
     return response.render('auth/login')
@@ -9,20 +16,32 @@ const login = (request, response) => {
 const loginPost = (request, response) => {
     return response.send('login post')
 }
+
 const signupPost = (request, response) => {
     const { email, password } = request.body
     const result = validationResult(request)
-
-    if (result.isEmpty()) {
+    const isEmailUnique = User.findOne({ email: email }).then((result) => console.log(result))
+    if (result.isEmpty() && isEmailUnique == null) {
         try {
             bcrypt.hash(password, 10).then((hash) => {
                 const user = new User({
                     email: email,
                     password: hash
                 })
-                user.save()
+                const savedUser = user.save()
+                console.log(savedUser)
+
+                const Newuser = User.findOne({ email })
+
+
+                const token = jwt.sign({ userId: Newuser._id }, 'your-secret-key', {
+                    expiresIn: '1h',
+                });
+
+                response.cookie('jwt', token, { maxAge: expiresAt * 1000, httpOnly: true })
+                response.status(201).send(token)
             })
-            return response.redirect('/')
+            // return response.redirect('/')
         } catch (error) {
             console.log(error.message)
         }
