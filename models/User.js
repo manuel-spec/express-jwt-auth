c1d1916ab98a7b7027db6b8b6ade2f459b96b3fe
manuel-spec <amaniase084@gmail.com>
2023-12-30 11:13:55 +0300

    database user model construction

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const { isEmail } = require('validator')
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "email is required !"],
+        unique: true,
+        validate: [isEmail, 'a valid email is required !']
+    },
+    password: {
+        type: String,
+        required: [true, 'password is required'],
+        minLength: [6, 'minimum password length is 6']
+    }
+}, { timestamps: true })
+
+const User = mongoose.model('User', userSchema)
